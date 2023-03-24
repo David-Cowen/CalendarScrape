41d28e765efab05056747f1c25fbaf588215a1a5
David Cowen <david.john.cowen@gmail.com>
2023-03-24 22:27:20 +1000

    Begins formatting structure with comments.

---
+++ CalendarScrape.R
@@ -0,0 +1,13 @@
+####------------------------------  Install Packages  ------------------------------####
+
+
+####------------------------------  Libraries         ------------------------------####
+
+
+
+####------------------------------  Source Files      ------------------------------####
+# Melbourne Institute - Latest News URL
+miURL = 'https://melbourneinstitute.unimelb.edu.au/publications/macroeconomic-reports/latest-news/index-of-consumer-sentiment'
+
+# Westpac PDF
+pdfFile = 'Economic_Calendar_March2023_WBC'
